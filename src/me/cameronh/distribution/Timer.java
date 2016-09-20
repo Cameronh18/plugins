e5f4a7e51246d34d8865748d879db2bdb41eb944
Cameronh <andreispas1998@gmail.com>
2016-09-20 12:08:52 +1000

    Update Timer.java

--- src/me/cameronh/distribution/Timer.java
+++ src/me/cameronh/distribution/Timer.java
@@ -1,23 +1,33 @@
 package me.cameronh.distribution;
 
+import me.cameronh.distribution.config.Message;
+
 import org.bukkit.Bukkit;
 
 public class Timer{
 	public static void getTimer(int time) {
 		if(time % 1200 == 0 && time > 1200) {
-			Bukkit.broadcastMessage("До начала раздачи осталось: " + time / 1200 + " минут");
+			int i = time / 1200;
+			Bukkit.broadcastMessage(Message.getConfig().getString("Message.Prefix") + Message.getConfig().getString("Message.DelayStartMinute").replace("{time}", "" + i));
 		}
 		else if(time % 600 == 0 && time == 1200) {
-			Bukkit.broadcastMessage("До начала раздачи осталось: " + time / 20 + " секунд");
+			int i = time / 20;
+			Bukkit.broadcastMessage(Message.getConfig().getString("Message.Prefix") + Message.getConfig().getString("Message.DelayStartSecond").replace("{time}", "" + i));
 		}
 		else if(time % 200 == 0 && time <= 600 && time > 200) {
-			Bukkit.broadcastMessage("До начала раздачи осталось: " + time / 20 + " секунд");
+			int i = time / 20;
+			Bukkit.broadcastMessage(Message.getConfig().getString("Message.Prefix") + Message.getConfig().getString("Message.DelayStartSecond").replace("{time}", "" + i));
 		}
-		else if(time % 20 == 0 && time <= 200 && time > 0) {
-			Bukkit.broadcastMessage("До начала раздачи осталось: " + time / 20 + " секунд");
+		else if(time % 100 == 0 && time <= 200 && time > 100) {
+			int i = time / 20;
+			Bukkit.broadcastMessage(Message.getConfig().getString("Message.Prefix") + Message.getConfig().getString("Message.DelayStartSecond").replace("{time}", "" + i));
+		}
+		else if(time % 20 == 0 && time <= 100 && time > 0) {
+			int i = time / 20;
+			Bukkit.broadcastMessage(Message.getConfig().getString("Message.Prefix") + Message.getConfig().getString("Message.DelayStartSecond").replace("{time}", "" + i));
 		}
 		if(time == 20) {
-			Bukkit.broadcastMessage("Раздача началась!");
+			Bukkit.broadcastMessage(Message.getConfig().getString("Message.Prefix") + Message.getConfig().getString("Message.Start"));
 		}
 	}
 }
